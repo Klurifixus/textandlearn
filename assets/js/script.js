b4d13dae160b1ff0b45e2b18ca8e4363b7eda037
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-07 11:57:01 +0200

    change shit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+console.log("I like Pizza");
+console.log("Its really good")
+
+window.alert("pizza is the shit")
+
+
+//this is a comment
+
+
+let firstname = "kalle"; //strings
+let age = 21;           //numbers but when you add: age = age + 1 (blir det en string)
+let student = true;     //booleans
+
+age = age +1;
+
+console.log("Hello", firstname);
+console.log("You are", age, "years old!")
+console.log("Enrolled:", student);
+
+document.getElementById("p1").innerHTML = "Hello" + firstname;
