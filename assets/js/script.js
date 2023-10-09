eee5a261cd7330598f93dae50005befcd8798961
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-10-09 09:32:15 +0200

    guided by BROCODE YOUTUBE CHANNEL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,45 @@
-console canvas = document.getElementById("canvas");
-const crx = canvas.getContext("2d");
+const playerText = document.querySelector("#playerText");
+const compuerText = document.querySelector("#computerText");
+const resultText = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtn");
+let player;
+let computer;
+let result;
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+   player = button.textContent;
+   computerTurn();
+   playerText.textContent = "Player: ${player}";
+   computerText.textContent = "Computer: ${computer}";
+   resultText.textContent = "Result: ${Result}";
 
-const CANVAS_WIDTH = canvas.width = 600;
-cost CANVAS_HEIGHT = canvas.height = 600;
 
-cost playerImage = new Image();
-playerImage.src = "./assets/images/assets/images/shadow_dog.png";
-const spriteWidth = 575;
-cost spriteHeight = 523;
-let frameX = 4;
-let frameY = 5;//byta frame
-let gameFrame = 0;
-cost staggerFrames = 5; //hastinghet
+}));
 
-
-function animate(){
-   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-   //ctx.fillRect(50,50,100,100);
-   //ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-   let position = Math.floor(gameFrame/staggeredFrames) % 6;
-   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spritewidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-   requestAnimationFrame(animate);
-   if (gameFrame % staggerFrames == 0){//hastighet
-   if (frameX < 6) frameX++;//antal frames 
-   else frameX = 0;
-
-    }
-   gameframe ++;
-};
-animated();
-
+function computerTurn(){
+   const randNum = Math.floor(Math.random() * 3) + 1;
+   switch(randNum){
+      case 1:
+         computer = "ROCK";
+         break;
+      case 2:
+         computer = "PAPER";
+         break;
+      case 3:
+            computer = "SCISSORS";
+            break;   
+         
+   }
+}
+function checkWinner(){
+   if(player == computer){
+      return "Draw!";  
+   } else if(computer == "ROCK"){
+      return (player == "PAPER") ? "YOU WIN!" : "YOU LOSE!"
+   }
+   else if(computer == "PAPER"){
+      return (player == "SCISSORS") ? "YOU WIN!" : "YOU LOSE!"
+   }
+   else if(computer == "SCISSORS"){
+      return (player == "ROCK") ? "YOU WIN!" : "YOU LOSE!"
+   }
+}
